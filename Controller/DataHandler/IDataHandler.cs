c5640e3f33989796fd8436b32095d87858e2d29d
Jonmb026 <156730783+Jonmb026@users.noreply.github.com>
2024-11-17 15:04:52 -0600

    Se crea la logica para el manejo de archivos JSON

--- Controller/DataHandler/IDataHandler.cs
+++ Controller/DataHandler/IDataHandler.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Controller.DataHandler
 {
-    internal class IDataHandler
+    /// <summary>
+    /// Interfaz para manejar datos genéricos.
+    /// </summary>
+    /// <typeparam name="T">El tipo de datos que se manejará.</typeparam>
+    public interface IDataHandler<T>
     {
+        /// <summary>
+        /// Obtiene todos los elementos de un archivo JSON.
+        /// </summary>
+        /// <returns>Lista de elementos del tipo especificado.</returns>
+        List<T> GetAll();
+
+        /// <summary>
+        /// Guarda todos los elementos en un archivo JSON.
+        /// </summary>
+        /// <param name="data">Lista de elementos a guardar.</param>
+        void SaveAll(List<T> data);
+
+        /// <summary>
+        /// Obtiene un elemento por su identificador único.
+        /// </summary>
+        /// <param name="id">El identificador único del elemento.</param>
+        /// <returns>El elemento encontrado, o null si no existe.</returns>
+        T GetById(string id);
     }
 }
+
