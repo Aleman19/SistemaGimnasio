9f2c038b05e895b58dbd4b4b58cc4e85b6c7a6e5
Aleman19 <mariaferale99@gmail.com>
2024-11-17 21:05:46 -0600

    Se corrige error de using

--- SistemaGimnasio/LoginForm.Designer.cs
+++ SistemaGimnasio/LoginForm.Designer.cs
@@ -15,82 +15,90 @@
 
         private void InitializeComponent()
         {
-            this.txtUsuario = new System.Windows.Forms.TextBox();
-            this.txtContraseña = new System.Windows.Forms.TextBox();
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.btnSalir = new System.Windows.Forms.Button();
-            this.lblUsuario = new System.Windows.Forms.Label();
-            this.lblContraseña = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            txtUsuario = new TextBox();
+            txtContraseña = new TextBox();
+            btnLogin = new Button();
+            btnSalir = new Button();
+            lblUsuario = new Label();
+            lblContraseña = new Label();
+            SuspendLayout();
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(120, 50);
-            this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(200, 20);
-            this.txtUsuario.TabIndex = 0;
+            txtUsuario.Location = new Point(140, 58);
+            txtUsuario.Margin = new Padding(4, 3, 4, 3);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(233, 23);
+            txtUsuario.TabIndex = 0;
             // 
             // txtContraseña
             // 
-            this.txtContraseña.Location = new System.Drawing.Point(120, 100);
-            this.txtContraseña.Name = "txtContraseña";
-            this.txtContraseña.PasswordChar = '*';
-            this.txtContraseña.Size = new System.Drawing.Size(200, 20);
-            this.txtContraseña.TabIndex = 1;
+            txtContraseña.Location = new Point(140, 115);
+            txtContraseña.Margin = new Padding(4, 3, 4, 3);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.PasswordChar = '*';
+            txtContraseña.Size = new Size(233, 23);
+            txtContraseña.TabIndex = 1;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(120, 150);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
-            this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "Iniciar Sesión";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            btnLogin.Location = new Point(140, 173);
+            btnLogin.Margin = new Padding(4, 3, 4, 3);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(88, 27);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Iniciar Sesión";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(245, 150);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
-            this.btnSalir.TabIndex = 3;
-            this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
-            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            btnSalir.Location = new Point(286, 173);
+            btnSalir.Margin = new Padding(4, 3, 4, 3);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(88, 27);
+            btnSalir.TabIndex = 3;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
             // 
             // lblUsuario
             // 
-            this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Location = new System.Drawing.Point(50, 53);
-            this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
-            this.lblUsuario.TabIndex = 4;
-            this.lblUsuario.Text = "Usuario:";
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(58, 61);
+            lblUsuario.Margin = new Padding(4, 0, 4, 0);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(50, 15);
+            lblUsuario.TabIndex = 4;
+            lblUsuario.Text = "Usuario:";
             // 
             // lblContraseña
             // 
-            this.lblContraseña.AutoSize = true;
-            this.lblContraseña.Location = new System.Drawing.Point(50, 103);
-            this.lblContraseña.Name = "lblContraseña";
-            this.lblContraseña.Size = new System.Drawing.Size(64, 13);
-            this.lblContraseña.TabIndex = 5;
-            this.lblContraseña.Text = "Contraseña:";
+            lblContraseña.AutoSize = true;
+            lblContraseña.Location = new Point(58, 119);
+            lblContraseña.Margin = new Padding(4, 0, 4, 0);
+            lblContraseña.Name = "lblContraseña";
+            lblContraseña.Size = new Size(70, 15);
+            lblContraseña.TabIndex = 5;
+            lblContraseña.Text = "Contraseña:";
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 250);
-            this.Controls.Add(this.lblContraseña);
-            this.Controls.Add(this.lblUsuario);
-            this.Controls.Add(this.btnSalir);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.txtContraseña);
-            this.Controls.Add(this.txtUsuario);
-            this.Name = "LoginForm";
-            this.Text = "Inicio de Sesión";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(467, 288);
+            Controls.Add(lblContraseña);
+            Controls.Add(lblUsuario);
+            Controls.Add(btnSalir);
+            Controls.Add(btnLogin);
+            Controls.Add(txtContraseña);
+            Controls.Add(txtUsuario);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "LoginForm";
+            Text = "Inicio de Sesión";
+            Load += LoginForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         private System.Windows.Forms.TextBox txtUsuario;
