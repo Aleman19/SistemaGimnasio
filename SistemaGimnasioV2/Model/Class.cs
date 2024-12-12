f79da5353a00c47a4810dded47eecd83893af206
Aleman19 <mariaferale99@gmail.com>
2024-12-12 01:00:13 -0600

    Se crea la logica de los modelos User, Membership, Class, Reservation

--- SistemaGimnasioV2/Model/Class.cs
+++ SistemaGimnasioV2/Model/Class.cs
@@ -1,6 +1,24 @@
-﻿namespace SistemaGimnasioV2.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestiónGimnasioMVC.Model
 {
     public class Class
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } // Nombre de la clase
+        [MaxLength(200)]
+        public string Description { get; set; } // Descripción de la clase
+        [Required]
+        public DateTime Schedule { get; set; } // Horario de la clase
+        [Required]
+        public int TrainerId { get; set; } // Relación con el entrenador
+
+        [ForeignKey("TrainerId")]
+        public User Trainer { get; set; } // Entrenador asignado
     }
 }
--- SistemaGimnasioV2/Model/Reservation.cs
+++ SistemaGimnasioV2/Model/Reservation.cs
@@ -1,6 +1,22 @@
-﻿namespace SistemaGimnasioV2.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace GestiónGimnasioMVC.Model
 {
     public class Reservation
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ClassId { get; set; } // Relación con la clase
+        [ForeignKey("ClassId")]
+        public Class GymClass { get; set; } // Clase asociada
+        [Required]
+        public int ClientId { get; set; } // Relación con el cliente
+        [ForeignKey("ClientId")]
+        public User Client { get; set; } // Cliente asociado
+
+        [Required]
+        public DateTime ReservationDate { get; set; } = DateTime.Now; // Fecha de la reserva
     }
 }
